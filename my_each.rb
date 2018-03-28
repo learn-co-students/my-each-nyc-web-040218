49eb1bf12fcf989d3d20a5c11b384fabcc7f4a02
eyi1 <ej7729@gmail.com>
2018-03-28 17:42:31 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (words)
+new=[]
+if block_given?
+
+    i = 0
+    while i<words.length
+      yield(words[i])
+      i=i+1
+    end
+  words[i]
+end
+
+  words.to_a# put argument(s) here
+  #code here
 end
